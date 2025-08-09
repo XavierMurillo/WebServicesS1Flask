1281a321e7b4e760276e762d34a46f69a99d00ca
XavierMurillo <xaviermurillosanchez@gmail.com>
2025-08-09 00:53:28 -0400

    Added HTML, and post data

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify, render_template
 
+# Se crea una aplicación flask
 app = Flask(__name__)
 
+# Se generan rutas según la necesidad del proyecto
 @app.route("/status", methods=["GET"])
 def status_data():
     return jsonify(
@@ -9,6 +11,16 @@ def status_data():
             "status":"todo esta mal"
         }
     )
+
+pulsaciones_globales = 0
+@app.route("/pressing", methods=["GET","POST"])
+def show_pressing():
+    global pulsaciones_globales
+    if request.method == "POST":
+        pulsaciones_globales = request.get_json().get('pulsaciones')
+        return render_template("mostrar_pulsaciones.html", pulsaciones= pulsaciones_globales)
+    else:
+        return render_template("mostrar_pulsaciones.html", pulsaciones= pulsaciones_globales)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
